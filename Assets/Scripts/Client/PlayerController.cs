1563b248eeaa44f9e706e84218122f1d3732c06f
Kate <kdemchyshyn@kse.org.ua>
2026-07-16 16:01:48 +0300

    fully putted highlight in client logic and updated prefabs. Hasn't moved validation from chunk manager - as it is server part of the rpc call

--- Assets/Scripts/Client/PlayerController.cs
+++ Assets/Scripts/Client/PlayerController.cs
@@ -1,140 +1,142 @@
-using System.Collections.Generic;
-using Mirror;
-using UnityEngine;
-using UnityEngine.Serialization;
+    using System.Collections.Generic;
+    using Mirror;
+    using UnityEngine;
 
-public class PlayerController : NetworkBehaviour
-{
-    public PlayerData playerData;
-    private PlayerRenderer _playerRenderer;
-    
-    private float _velocityY = 0f;
-    private bool _isGrounded = false;
-
-    private ChunkManager _chunkManager;
-    private BlockHighlight _blockHighlight;
-
-    void Start()
+    public class PlayerController : NetworkBehaviour
     {
-        _chunkManager = ChunkManager.Instance;
-        _playerRenderer = GetComponent<PlayerRenderer>();
-        _blockHighlight = FindObjectOfType<BlockHighlight>();
-       
-        if (!isLocalPlayer) return;
+        public PlayerData playerData;
+        private PlayerRenderer _playerRenderer;
         
-        SubscribeToChunks();
-    }
+        private float _velocityY = 0f;
+        private bool _isGrounded = false;
 
-    private List<NetworkConnectionToClient> _chunkListeners = new();
-    
-    [Command]
-    void SubscribeToChunks()
-    {
-        _chunkListeners.Add(connectionToClient);
-    }
+        private ChunkManager _chunkManager;
+        private BlockHighlight _blockHighlight;
 
-    private int? place = null;
-    void Update()
-    {
-        // Safety check: We only want the player who OWNS this object to send inputs.
-        if (!isLocalPlayer) return;
-        
-        if (Input.GetKeyDown(KeyCode.LeftAlt))
+        void Start()
         {
-            _blockHighlight.ChangeMode();
-        }
-        
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            place = 0;
-            _blockHighlight.SetBlockId((ushort)place);
-        }
-        
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            place = 1;
-            _blockHighlight.SetBlockId((ushort)place);
+            _chunkManager = ChunkManager.Instance;
+            _playerRenderer = GetComponent<PlayerRenderer>();
+            _blockHighlight = GetComponent<BlockHighlight>();
+            
+            _blockHighlight.enabled = true;
+            _blockHighlight.InitializeHighlight();
+           
+            if (!isLocalPlayer) return;
+            
+            SubscribeToChunks();
         }
 
-        if (Input.GetMouseButton(0) && place != null)
+        private List<NetworkConnectionToClient> _chunkListeners = new();
+        
+        [Command]
+        void SubscribeToChunks()
         {
-            Vector3 mousePos = Input.mousePosition;
-            // Ensure ScreenToWorldPoint works correctly by providing distance from camera
-            mousePos.z = -Camera.main.transform.position.z; 
+            _chunkListeners.Add(connectionToClient);
+        }
+
+        private int? place = null;
+        void Update()
+        {
+            // Safety check: We only want the player who OWNS this object to send inputs.
+            if (!isLocalPlayer) return;
             
-            Vector3 worldCoord = Camera.main.ScreenToWorldPoint(mousePos);
-            worldCoord.z = 0f;
-            
-            Vector3Int cellPos = _chunkManager.playerGrid.WorldToCell(worldCoord);
-            
-            // Only place blocks if we are clicking INSIDE the chunk boundaries (0 to 63)
-            if (cellPos.x >= 0 && cellPos.x < ChunkUtils.ChunkSize && 
-                cellPos.y >= 0 && cellPos.y < ChunkUtils.ChunkSize)
+            if (Input.GetKeyDown(KeyCode.LeftAlt))
             {
-                _chunkManager.Place((byte)cellPos.x, (byte)cellPos.y, new BlockID((ushort)place.Value));
+                _blockHighlight.ChangeMode();
             }
-        }
-        
-        // When the local player presses Space, ask the server to change the color.
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            _playerRenderer.ChangeColor();
-        }
-        
-        float control = 0f;
-        if (Input.GetKey(KeyCode.A))
-        {
-            control += -1f;
-            _playerRenderer.ChangeDirection(true);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            control += 1f;
-            _playerRenderer.ChangeDirection(false);
-        }
-        
-        bool wantsToJump = Input.GetKeyDown(KeyCode.Space);
-
-        if (_isGrounded)
-        {
-            //on the ground -> no gravity
-            _velocityY = 0f;
-            //jump -> impulse up
-            if (wantsToJump)
+            
+            if (Input.GetKeyDown(KeyCode.Alpha1))
             {
-                _velocityY = playerData.jumpForce;
-                _isGrounded = false;
+                place = 0;
+                _blockHighlight.SetBlockId((ushort)place);
             }
-        }
-        else
-        {
-            //gravity pulls down
-            _velocityY += playerData.gravity * Time.deltaTime;
-        }
-        
-        Vector3 movement = new Vector3(control * playerData.baseSpeed, _velocityY, 0f);
-        
-        GetComponent<Transform>().position += movement  * Time.deltaTime;
-        
-        //tmp floor
-        if (transform.position.y <= 41.3f)
-        {
-            transform.position = new Vector3(transform.position.x, 41.3f, transform.position.z);
-            _isGrounded = true; 
-        }
-        
-        //CmdAffectPos(control);
-    }
-    
-    [Command]
-    void CmdAffectPos(float pos)
-    {
-        
-    }
+            
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                place = 1;
+                _blockHighlight.SetBlockId((ushort)place);
+            }
 
-    [ClientRpc]
-    void RpcLogChange(string message)
-    {
-        Debug.Log($"[Server says]: {message}");
+            if (Input.GetMouseButton(0) && place != null)
+            {
+                Vector3 mousePos = Input.mousePosition;
+                // Ensure ScreenToWorldPoint works correctly by providing distance from camera
+                mousePos.z = -Camera.main.transform.position.z; 
+                
+                Vector3 worldCoord = Camera.main.ScreenToWorldPoint(mousePos);
+                worldCoord.z = 0f;
+                
+                Vector3Int cellPos = _chunkManager.playerGrid.WorldToCell(worldCoord);
+                
+                // Only place blocks if we are clicking INSIDE the chunk boundaries (0 to 63)
+                if (cellPos.x >= 0 && cellPos.x < ChunkUtils.ChunkSize && 
+                    cellPos.y >= 0 && cellPos.y < ChunkUtils.ChunkSize)
+                {
+                    _chunkManager.Place((byte)cellPos.x, (byte)cellPos.y, new BlockID((ushort)place.Value));
+                }
+            }
+            
+            // When the local player presses Space, ask the server to change the color.
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                _playerRenderer.ChangeColor();
+            }
+            
+            float control = 0f;
+            if (Input.GetKey(KeyCode.A))
+            {
+                control += -1f;
+                _playerRenderer.ChangeDirection(true);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                control += 1f;
+                _playerRenderer.ChangeDirection(false);
+            }
+            
+            bool wantsToJump = Input.GetKeyDown(KeyCode.Space);
+
+            if (_isGrounded)
+            {
+                //on the ground -> no gravity
+                _velocityY = 0f;
+                //jump -> impulse up
+                if (wantsToJump)
+                {
+                    _velocityY = playerData.jumpForce;
+                    _isGrounded = false;
+                }
+            }
+            else
+            {
+                //gravity pulls down
+                _velocityY += playerData.gravity * Time.deltaTime;
+            }
+            
+            Vector3 movement = new Vector3(control * playerData.baseSpeed, _velocityY, 0f);
+            
+            GetComponent<Transform>().position += movement  * Time.deltaTime;
+            
+            //tmp floor
+            if (transform.position.y <= 41.3f)
+            {
+                transform.position = new Vector3(transform.position.x, 41.3f, transform.position.z);
+                _isGrounded = true; 
+            }
+            
+            //CmdAffectPos(control);
+        }
+        
+        [Command]
+        void CmdAffectPos(float pos)
+        {
+            
+        }
+
+        [ClientRpc]
+        void RpcLogChange(string message)
+        {
+            Debug.Log($"[Server says]: {message}");
+        }
     }
-}
